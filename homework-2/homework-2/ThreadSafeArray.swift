7a6fe7e188f8c118a0fdc429cc1dd938d80d5ba4
alecsmirnov <alecsmirnov93@gmail.com>
2020-10-23 14:02:14 +0700

    Релизована часть протоколов массива

--- homework-2/homework-2/ThreadSafeArray.swift
+++ homework-2/homework-2/ThreadSafeArray.swift
@@ -7,66 +7,124 @@
 
 import Foundation
 
-final class ThreadSafeArray<T: Equatable> {
+final class ThreadSafeArray<T> {
+    typealias ThreadSafeArrayType = [T]
+    
+    typealias Element = ThreadSafeArrayType.Element
+    typealias Index = ThreadSafeArrayType.Index
+    typealias Iterator = ThreadSafeArrayType.Iterator
+    
+    private var data = ThreadSafeArrayType()
+    private let queue = DispatchQueue(label: "ThreadSafeArrayQueue", attributes: .concurrent)
+}
+
+extension ThreadSafeArray where Element: Equatable {
+    func contains(_ element: Element) -> Bool {
+        var contains = false
+
+        queue.sync {
+            contains = data.contains(element)
+        }
+
+        return contains
+    }
+}
+
+// MARK: - Collection
+
+extension ThreadSafeArray: Collection {
     var isEmpty: Bool {
         var isEmpty = false
-        
+
         queue.sync {
             isEmpty = data.isEmpty
         }
-        
+
         return isEmpty
     }
-    
+
     var count: Int {
         var count = 0
-        
+
         queue.sync {
             count = data.count
         }
-        
+
         return count
     }
-    
-    private var data = [T]()
-    private let queue = DispatchQueue(label: "ThreadSafeArrayQueue", attributes: .concurrent)
-    
-    func append(_ item: T) {
-        queue.async(flags: .barrier) {
-            self.data.append(item)
-        }
-    }
-    
-    func remove(at index: Int) {
-        queue.async(flags: .barrier) {
-            self.data.remove(at: index)
-        }
-    }
-    
-    func contains(_ item: T) -> Bool {
-        var contains = false
-        
+
+    var startIndex: Index {
         queue.sync {
-            contains = data.contains(item)
+            return data.startIndex
         }
-        
-        return contains
     }
-    
-    subscript(index: Int) -> T {
+
+    var endIndex: Index {
+        queue.sync {
+            data.endIndex
+        }
+    }
+
+    func index(after i: Index) -> Index {
+        queue.sync {
+            return data.index(after: i)
+        }
+    }
+
+    subscript(index: Index) -> Element {
         get {
-            var item: T!
-            
+            var element: Element!
+
             queue.sync {
-                item = self.data[index]
+                element = self.data[index]
             }
-            
-            return item
+
+            return element
         }
         set {
             queue.async(flags: .barrier) {
                 self.data[index] = newValue
             }
         }
+    }
+}
+
+extension ThreadSafeArray: RangeReplaceableCollection {
+    func append(_ element: Element) {
+        queue.async(flags: .barrier) {
+            self.data.append(element)
+        }
+    }
+        
+    func remove(at index: Index) -> Element {
+        var element: Element!
+        
+        queue.sync {
+            element = data.remove(at: index)
+        }
+        
+        return element
+    }
+}
+
+// MARK: - ExpressibleByArrayLiteral
+
+extension ThreadSafeArray: ExpressibleByArrayLiteral {
+    convenience init(arrayLiteral elements: Element...) {
+        self.init()
+        
+        for element in elements {
+            queue.async(flags: .barrier) {
+                self.data.append(element)
+            }
+        }
+    }
+}
+
+// MARK: - Sequence
+
+extension ThreadSafeArray: Sequence {
+    func makeIterator() -> Iterator {
+        return data.makeIterator()
     }
 }
