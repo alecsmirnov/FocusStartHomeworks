b86a45efac0504afd37aeda748290639d5aecb49
alecsmirnov <alecsmirnov93@gmail.com>
2020-10-29 21:48:25 +0700

    Проект третьей ДЗ добавлен в воркспейс

---
+++ homework-3/homework-3/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  homework-3
+//
+//  Created by Admin on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
