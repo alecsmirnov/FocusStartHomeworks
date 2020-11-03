b8689689c813e23bcb82b14b5c0bb87de23cb270
alecsmirnov <alecsmirnov93@gmail.com>
2020-11-03 22:04:57 +0700

    Создан проект для д/р 4

---
+++ homework-4/homework-4/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  homework-4
+//
+//  Created by Admin on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
