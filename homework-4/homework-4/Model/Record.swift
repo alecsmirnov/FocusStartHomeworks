3715b9187890061ae8d384c8e417b173a7258d34
alecsmirnov <alecsmirnov93@gmail.com>
2020-11-04 17:16:32 +0700

    Добавлены картинки в модель

--- homework-4/homework-4/Model/Record.swift
+++ homework-4/homework-4/Model/Record.swift
@@ -5,10 +5,14 @@
 //  Created by Admin on 04.11.2020.
 //
 
-import Foundation
+import UIKit
 
 struct Record {
     let title: String
+    
     var text: String?
     var date: Date?
+    
+    var firstImage: UIImage?
+    var secondImage: UIImage?
 }
