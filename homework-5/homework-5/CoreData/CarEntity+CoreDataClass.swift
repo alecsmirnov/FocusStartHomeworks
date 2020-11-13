0027b87e61a77d518498134903e0c1b9aac22bf1
alecsmirnov <alecsmirnov93@gmail.com>
2020-11-13 23:53:49 +0700

    Добавлена CoreData

---
+++ homework-5/homework-5/CoreData/CarEntity+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  CarEntity+CoreDataClass.swift
+//  homework-5
+//
+//  Created by Admin on 13.11.2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CarEntity)
+public class CarEntity: NSManagedObject {}
--- homework-5/homework-5/Services/CarService.swift
+++ homework-5/homework-5/Services/CarService.swift
@@ -5,6 +5,9 @@
 //  Created by Admin on 16.10.2020.
 //
 
+import UIKit
+import CoreData
+
 final class CarService {
     var isEmpty: Bool {
         return filteredData.isEmpty
@@ -14,11 +17,44 @@ final class CarService {
         return filteredData.count
     }
     
-    private var filteredData = [Car]()
-    private var tempData = [Car]()
+    private var filteredData = [CarEntity]()
+    private var tempData = [CarEntity]()
+    
+    private var managedContext: NSManagedObjectContext
+
+    init() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<CarEntity> = CarEntity.fetchRequest()
+
+        do {
+            filteredData = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            fatalError("could not fetch. \(error), \(error.userInfo)")
+        }
+    }
     
     func append(car: Car) {
-        filteredData.append(car)
+        let carEntity = CarEntity(context: managedContext)
+        
+        carEntity.manufacturer = car.manufacturer
+        carEntity.model = car.model
+        
+        if let yearOfIssue = car.yearOfIssue {
+            carEntity.yearOfIssue = NSNumber(value: yearOfIssue)
+        }
+        
+        carEntity.body = car.body.rawValue
+        carEntity.carNumber = car.carNumber
+        
+        filteredData.append(carEntity)
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            fatalError("could not save. \(error), \(error.userInfo)")
+        }
     }
     
     func get(at index: Int) -> Car {
@@ -26,7 +62,15 @@ final class CarService {
             fatalError("index is out of range")
         }
         
-        return filteredData[index]
+        guard let body = Body(rawValue: filteredData[index].body) else {
+            fatalError("wrong body type")
+        }
+        
+        return Car(manufacturer: filteredData[index].manufacturer,
+                   model: filteredData[index].model,
+                   body: body,
+                   yearOfIssue: filteredData[index].yearOfIssue?.intValue,
+                   carNumber: filteredData[index].carNumber)
     }
     
     func remove(at index: Int) {
@@ -35,6 +79,19 @@ final class CarService {
         }
         
         filteredData.remove(at: index)
+        
+        let fetchRequest: NSFetchRequest<CarEntity> = CarEntity.fetchRequest()
+
+        do {
+            let entities = try managedContext.fetch(fetchRequest)
+            let entityObject = entities[index] as NSManagedObject
+                
+            managedContext.delete(entityObject)
+                
+            try managedContext.save()
+        } catch let error as NSError {
+            fatalError("could not fetch. \(error), \(error.userInfo)")
+        }
     }
     
     func replace(at index: Int, with car: Car) {
@@ -42,7 +99,21 @@ final class CarService {
             fatalError("index is out of range")
         }
         
-        filteredData[index] = car
+        filteredData[index].manufacturer = car.manufacturer
+        filteredData[index].model = car.model
+        
+        if let yearOfIssue = car.yearOfIssue {
+            filteredData[index].yearOfIssue = NSNumber(value: yearOfIssue)
+        }
+        
+        filteredData[index].body = car.body.rawValue
+        filteredData[index].carNumber = car.carNumber
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            fatalError("could not fetch. \(error), \(error.userInfo)")
+        }
     }
     
     func filterReset() {
@@ -57,13 +128,27 @@ final class CarService {
             tempData += filteredData
         }
         
-        filteredData = tempData.filter { $0.body == body }
+        filteredData = tempData.filter { $0.body == body.rawValue }
         tempData = tempData.filter { !filteredData.contains($0) }
     }
     
     func removeAll() {
         filteredData.removeAll()
         tempData.removeAll()
+        
+        let fetchRequest: NSFetchRequest<CarEntity> = CarEntity.fetchRequest()
+
+        do {
+            let entities = try managedContext.fetch(fetchRequest)
+
+            for entity in entities {
+                let entityObject = entity as NSManagedObject
+
+                managedContext.delete(entityObject)
+            }
+        } catch let error as NSError {
+            fatalError("could not fetch. \(error), \(error.userInfo)")
+        }
     }
     
     deinit {
@@ -77,31 +162,33 @@ extension CarService {
     static func mockData() -> CarService {
         let carService = CarService()
         
-        let car1 = Car(
-            manufacturer: "Ford",
-            model: "Shelby GT500",
-            body: Body.coupe,
-            yearOfIssue: 2001,
-            carNumber: "A000TA"
-        )
-        let car2 = Car(
-            manufacturer: "Porsche",
-            model: "911",
-            body: Body.cabriolet,
-            yearOfIssue: 2016,
-            carNumber: "B023BA"
-        )
-        let car3 = Car(
-            manufacturer: "Lexus",
-            model: "LX 570",
-            body: Body.pickup,
-            yearOfIssue: 2020,
-            carNumber: "M294AK"
-        )
+        if carService.isEmpty {
+            let car1 = Car(
+                manufacturer: "Ford",
+                model: "Shelby GT500",
+                body: Body.coupe,
+                yearOfIssue: 2001,
+                carNumber: "A000TA"
+            )
+            let car2 = Car(
+                manufacturer: "Porsche",
+                model: "911",
+                body: Body.cabriolet,
+                yearOfIssue: 2016,
+                carNumber: "B023BA"
+            )
+            let car3 = Car(
+                manufacturer: "Lexus",
+                model: "LX 570",
+                body: Body.pickup,
+                yearOfIssue: 2020,
+                carNumber: "M294AK"
+            )
 
-        carService.append(car: car1)
-        carService.append(car: car2)
-        carService.append(car: car3)
+            carService.append(car: car1)
+            carService.append(car: car2)
+            carService.append(car: car3)
+        }
         
         return carService
     }
